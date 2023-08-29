887e058d7c03d27089976c79dfcaa48ad7f909c0
Pavel <zapasf@bk.ru>
2023-08-29 16:59:42 +0300

    add homework

---
+++ test_items.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pytest
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_basket(browser):
+    browser.get(link)
+    time.sleep(3)
+    buttons = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(buttons) > 0, 'button not found'
+
