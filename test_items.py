cdc3abac7dec96ae03ede32434d4acb465c0c9c4
Pavel <zapasf@bk.ru>
2023-08-29 17:11:31 +0300

    add homework redact

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_basket(browser):
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     buttons = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert len(buttons) > 0, 'button not found'
 
